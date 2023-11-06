83f6be9e20fa69de37d35d310acfbf3164d54a3c
Kyrylo Bondarenko <kirillbondarenkorino@gmail.com>
2023-11-06 17:07:15 +0200

    Added initial implementation of window manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
 
+#include "Managers/WindowsManager/windowsmanager.h"
 
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-    QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/qml/ui/MainRootWindow.qml"));
-    engine.load(url);
+    QGuiApplication app(argc, argv);
+    WindowsManager windowsManager;
 
     return app.exec();
 }
